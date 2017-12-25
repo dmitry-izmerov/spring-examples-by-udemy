5fca0fc818f0fbb6d26ae61651acbf6f5096fb98
Dmitrii_Izmerov <Dmitrii_Izmerov@epam.com>
2017-12-25 14:26:02 +0300

    xml config example with component scan

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -4,15 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import ru.demi.spring.ctrls.HelloCtrl;
 
 @SpringBootApplication
-@ComponentScan("ru.demi.spring")
-public class DependencyInjectionApplication {
+//@ComponentScan("ru.demi.spring")
+@ImportResource("classpath:spring-config.xml")
+public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
 		HelloCtrl helloCtrl = (HelloCtrl) ctx.getBean("helloCtrl");
 		helloCtrl.sayHello();
 	}
